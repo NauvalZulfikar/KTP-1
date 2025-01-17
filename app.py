93a7e10f2105a614784f45481f993e95669d9e23
brndwrlsqnchu7vrsaakpr <70472247+NauvalZulfikar@users.noreply.github.com>
2025-01-17 09:37:53 +0000

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from modify import modify
 from product_list_change import product_list_change
 from product_catalogue import product_catalogue
 # from scheduler import df, dfm, product_waiting_df, component_waiting_df, late_df
-
+a
 
 # if "df" not in st.session_state:
 #     st.session_state.dfm = df
